73dc5e99ee966c4ce9d3b545f4344db6064b7784
Marcos Baia <cs.marcosbaia@gmail.com>
2021-07-31 00:33:47 -0300

    Update desafio 004.py

--- desafio 004.py
+++ desafio 004.py
@@ -1,1 +1,13 @@
 # Desafio 004 - Faça um programa que leia algo pelo teclado e mostre na tela o seu tipo primitivo e todas as informações possiveis sobre ele.
+print('')
+algo = input('Digite algo: ')
+
+print('................................................................')
+print('O valor digitado é do tipo primitivo?        >> ', type(algo))
+print('O valor digitado só contém espaço?           >> ', algo.isspace())
+print('O valor digitado é um número?                >> ', algo.isnumeric())
+print('O valor digitado pertence ao alfabeto?       >> ', algo.isalpha())
+print('O valor digitado esta em letras maiusculas?  >> ', algo.isupper())
+print('O valor digitado esta em letras minusculas?  >> ', algo.islower())
+print('O valor digitado esta capitalizado?          >> ', algo.istitle())
+print('................................................................')
