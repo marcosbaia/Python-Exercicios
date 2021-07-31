c89acfe9c225a278832a3c717da14ebb9c51e227
Marcos Baia <cs.marcosbaia@gmail.com>
2021-07-30 21:02:44 -0300

    Create desafio 003.py

---
+++ desafio 003.py
@@ -0,0 +1,7 @@
+# Desafio 003 - Crie um programa que leia dois números e mostre a soma entre eles.
+numero1 = int(input('Digite um números: '))
+numero2 = int(input('Digite outro números: '))
+
+soma = numero1 + numero2
+
+print('O valor da soma de {} + {} é: {}'.format(numero1, numero2, soma))
