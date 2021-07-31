aa6b9c3ca191fa99aaac311a815644ffacc40ade
Marcos Baia <cs.marcosbaia@gmail.com>
2021-07-30 21:02:48 -0300

    Update desafio 002.py

--- desafio 002.py
+++ desafio 002.py
@@ -1,2 +1,4 @@
-nome = input("Digite seu nome: ")
-print("Seja bem vindo(a) {}!".format(nome))
+# Desafio 002 - Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas vindas.
+nome = input('Digite seu nome: ')
+
+print('Seja bem vindo(a) {}!'.format(nome))
