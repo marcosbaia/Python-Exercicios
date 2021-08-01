1b505d44c7d6b146f18701ef6727d04ec19f8218
Marcos Baia <cs.marcosbaia@gmail.com>
2021-08-01 01:11:46 -0300

    Create desafio 007.py

---
+++ desafio 007.py
@@ -0,0 +1,6 @@
+# Desafio 007 - Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média.
+nota1 = int(input('\nDigite a 1º nota: '))
+nota2 = int(input('Digite a 2º nota: '))
+media = (nota1 + nota2)/2
+
+print('\nA média da nota do aluno é: {} \n'.format(media))
