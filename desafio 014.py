8997b8ec0a98cb4441bc6f43c6e766aa83411397
Marcos Baia <cs.marcosbaia@gmail.com>
2021-08-16 16:35:00 -0300

    Update desafio 014.py

--- desafio 014.py
+++ desafio 014.py
@@ -1,6 +1,5 @@
 # Desafio 014 - Escreva um programa que converta uma temperatura digitada em celsius para Fahrenheit.
 celsius = float(input('\nDigite a temperatura em Celsius: '))
-fahrenheit = (celsius * 1.8) + 32
-
-print('\nA temperatura {}ºC em Celsius corresponde a {}ºF em Fahrenheit.\n'.format(celsius, fahrenheit))
-
+fahrenheit = ((9 * celsius)/ 5) + 32 
+ 
+print ('A temperatura {}°C em Celsius corresponde a {}°F em Fahrenheit\n'.format(celsius, fahrenheit))
