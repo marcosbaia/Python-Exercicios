48c099660e8472e3b4380ba46e5554d986c65e57
Marcos Baia <cs.marcosbaia@gmail.com>
2021-08-16 16:37:01 -0300

    Update desafio 015.py

--- desafio 015.py
+++ desafio 015.py
@@ -1,6 +1,7 @@
 # Desafio 015 - Escreva um programa que pergunte a quantidade de Km percorridos por um carro alugado e a quantidade de dias pelos quais ele foi alugado. Calcule o preço a pagar, sabendo que o carro custa R$60 por dia e R$0,15 por Km rodado.
-dias = float(input('\nQuantos dias o carro foi Aluguado: '))
-km = float(input('Quantos Km o carro pecorreu: '))
-valor = (dias * 60) + (km * 0.15)
+dias = int(input('Quantos dias o carro foi Alugado: '))
+km = float(input('Quantos km o carro Percorreu:'))
 
-print('\nO total a ser pago é de R$ {:.2f}\n'.format(valor))
+pagar = (dias * 60) + (km * 0.15)
+print('O valor a ser pago pelo Aluguel do carro é R${:.2f}'.format(pagar))
+
